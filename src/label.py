68772a782ed862661df2feff0880348fa62baa73
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-07 00:43:33 +0900

    関数化?

--- src/label.py
+++ src/label.py
@@ -1,6 +1,7 @@
 import glob, os, shutil, random, re
 import subprocess
 
+<<<<<<< HEAD
 image_path = "/path/"
 label_path = "/path/"
 
@@ -27,3 +28,31 @@ for path_file in glob.glob(os.path.join(image_path, "*.png")):
 
         #label = re.sub(re.compile(".jpg"), ".txt", image)
         shutil.copyfile(label_path + label_base + '.txt', label_path + image + ".txt")
+=======
+def add_label(image_path, label_path):
+
+    image_data = os.listdir(image_path)
+
+    for path_file in glob.glob(os.path.join(image_path, "*.png")):  
+
+        #画像fileの名前のみ取り出し
+        image, ext = os.path.splitext(os.path.basename(path_file))
+
+        #画像の名前と一致する名前のラベルを探す
+        if os.path.isfile(label_path + image + ".txt") is False:
+        
+            #水増し前の大本の画像のラベル取り出し
+            image_base = image.replace("_X", "") 
+            label_base, ext = os.path.splitext(os.path.basename(label_path + image_base + ".txt"))
+            print(label_base + "           label_base")
+
+            shutil.copyfile(label_path + label_base + '.txt', label_path + image + ".txt")
+
+if __name__ == '__main__':
+
+    #画像保存先とラベル保存先指定
+    image_path = "/path/"
+    label_path = "/path/"
+
+    add_label(image_path, label_path)
+>>>>>>> private
--- src/lumine.py
+++ src/lumine.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob, os, sys, re
 import cv2
+<<<<<<< HEAD
 import datatime
 
 file_path = "/your/directory/path/"
@@ -27,3 +28,29 @@ for i, src in enumerate(files):
     cv2.imwrite(reimage, img)
 
 
+=======
+
+def lumine(file_path):
+
+    for pathAndFilename in glob.iglob(os.path.join(file_path, "*.png")):
+
+        image, ext = os.path.splitext(os.path.basename(pathAndFilename))
+        src = cv2.imread(image + '.png')
+
+        #標準偏差32,平均120に変更
+        #if src is not None:
+        #    img = (src-np.mean(src))/np.std(src)*32+120
+        #    cv2.imwrite(image + '_1.png', img)
+
+        #標準偏差16,平均120に変更
+        if src is not None:
+            img = (src-np.mean(src))/np.std(src)*16+120
+            cv2.imwrite(image + '_X.png', img)
+
+if __name__ == '__main__':
+
+    #現在スクリプトが存在するディレクトリのパスを取り出し
+    file_path = os.path.dirname(os.path.abspath(__file__))
+
+    lumine(file_path)
+>>>>>>> private
