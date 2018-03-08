3b97330321f0927151c2aafb3e420cf9a1981cd1
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-08 16:47:11 +0900

    修正

--- src/label.py
+++ src/label.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import glob, os, shutil, random, re
 import subprocess
 
@@ -5,7 +8,7 @@ def add_label(image_path, label_path):
 
     image_data = os.listdir(image_path)
 
-    for path_file in glob.glob(os.path.join(image_path, "*.png")):  
+    for path_file in glob.glob(os.path.join(image_path, "*.jpg")):  
 
         #画像fileの名前のみ取り出し
         image, ext = os.path.splitext(os.path.basename(path_file))
@@ -13,17 +16,20 @@ def add_label(image_path, label_path):
         #画像の名前と一致する名前のラベルを探す
         if os.path.isfile(label_path + image + ".txt") is False:
         
+            print(image)
             #水増し前の大本の画像のラベル取り出し
-            image_base = image.replace("_X", "") 
+            image_base = image.replace('_A', '') 
+            image_base = image_base.replace('_B', '')
+            print(image_base)
             label_base, ext = os.path.splitext(os.path.basename(label_path + image_base + ".txt"))
-            print(label_base + "           label_base")
+            #print(label_base + "           label_base")
 
             shutil.copyfile(label_path + label_base + '.txt', label_path + image + ".txt")
 
 if __name__ == '__main__':
 
     #画像保存先とラベル保存先指定
-    image_path = "/path/"
-    label_path = "/path/"
+    image_path = "//"
+    label_path = "//"
 
     add_label(image_path, label_path)
--- src/lumine.py
+++ src/lumine.py
@@ -1,27 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import glob, os, sys, re
 import cv2
 
 def lumine(file_path):
 
-    for pathAndFilename in glob.iglob(os.path.join(file_path, "*.png")):
+    print("!!!!!!!!!!")
+    for pathAndFilename in glob.iglob(os.path.join(file_path, "*.jpg")):
 
         image, ext = os.path.splitext(os.path.basename(pathAndFilename))
-        src = cv2.imread(image + '.png')
+        src = cv2.imread(image + '.jpg')
 
         #標準偏差32,平均120に変更
-        #if src is not None:
-        #    img = (src-np.mean(src))/np.std(src)*32+120
-        #    cv2.imwrite(image + '_1.png', img)
+        if src is not None:
+            img = (src-np.mean(src))/np.std(src)*32+120
+            cv2.imwrite(image + '_A' + '.jpg', img)
 
         #標準偏差16,平均120に変更
         if src is not None:
             img = (src-np.mean(src))/np.std(src)*16+120
-            cv2.imwrite(image + '_X.png', img)
+            cv2.imwrite(image + '_B' + '.jpg', img)
 
 if __name__ == '__main__':
 
     #現在スクリプトが存在するディレクトリのパスを取り出し
-    file_path = os.path.dirname(os.path.abspath(__file__))
+    #file_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = "//"
 
     lumine(file_path)
