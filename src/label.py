0219017fd642a7859a92b8a3481f7526972c2770
Ryota9000 <s16c1044sc@s.chibakoudai.jp>
2018-03-09 18:17:14 +0900

    label.py 修正

--- src/label.py
+++ src/label.py
@@ -1,35 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import glob, os, shutil, random, re
 import subprocess
 
-def add_label(image_path, label_path):
+image_path = "/inflation/"
+label_path = "/labels/"
 
-    image_data = os.listdir(image_path)
+image_data = os.listdir(image_path)
 
-    for path_file in glob.glob(os.path.join(image_path, "*.jpg")):  
+for path_file in glob.glob(os.path.join(image_path, "*")):  
 
-        #画像fileの名前のみ取り出し
-        image, ext = os.path.splitext(os.path.basename(path_file))
+    #画像fileの名前のみ取り出し
+    image, ext = os.path.splitext(os.path.basename(path_file))
 
-        #画像の名前と一致する名前のラベルを探す
+    #labelを探す
+    #label = re.sub(re.compile(".jpg"), ".txt", image)
+    #label = glob.glob(label_path + image + ".txt")
+    #print(image)
+    #if str(ext) == ".jpg" or str(ext) == ".png":
+    if str(ext) == ".jpg":
+
         if os.path.isfile(label_path + image + ".txt") is False:
         
-            print(image)
             #水増し前の大本の画像のラベル取り出し
-            image_base = image.replace('_A', '') 
-            image_base = image_base.replace('_B', '')
-            print(image_base)
+            image_base = image.replace("_V", "") 
+            image_base = image_base.replace("_X", "") 
+            image_base = image_base.replace("_Ge", "") 
+            image_base = image_base.replace("_G", "") 
+            #print(image_base)
+            #label_base = glob.glob(label_path + image_base + ".txt")
             label_base, ext = os.path.splitext(os.path.basename(label_path + image_base + ".txt"))
-            #print(label_base + "           label_base")
+            print(label_base + "           label_base")
 
+            #label = re.sub(re.compile(".jpg"), ".txt", image)
             shutil.copyfile(label_path + label_base + '.txt', label_path + image + ".txt")
-
-if __name__ == '__main__':
-
-    #画像保存先とラベル保存先指定
-    image_path = "//"
-    label_path = "//"
-
-    add_label(image_path, label_path)
