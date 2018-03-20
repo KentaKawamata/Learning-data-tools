8e9d41ca73e2916833dce574109d78d4d1a0e2b5
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-20 17:18:06 +0900

    変数整理, 関数化

--- src/invert.py
+++ src/invert.py
@@ -1,48 +1,51 @@
 import glob, os, shutil, random, re
 import cv2
 
-image_dir = "/home/image/testimage/"
-label_dir = "/home/image/testlabel/"
+def reverse():
 
-for pathAndFile in glob.iglob(os.path.join(image_dir, "*")):
-    image, ext = os.path.splitext(os.path.basename(pathAndFile))
+    image_dir = "/home/image/testimage/"
+    label_dir = "/home/image/testlabel/"
 
-    print(ext)
-    if str(ext) == ".png":
-        src = cv2.imread(image_dir + image + '.png')
-    elif str(ext) == ".jpg":
-        src = cv2.imread(image_dir + image + '.jpg')
-    else:
-        src = None
+    for pathAndFile in glob.iglob(os.path.join(image_dir, "*")):
+        image, ext = os.path.splitext(os.path.basename(pathAndFile))
+
+        if str(ext) == ".png":
+            src = cv2.imread(image_dir + image + '.png')
+        elif str(ext) == ".jpg":
+            src = cv2.imread(image_dir + image + '.jpg')
+        else:
+            src = None
   
-    if src is not None:
-        inverse = cv2.flip(src, 1)
-        cv2.imwrite(image_dir + image + '_in' + '.jpg', inverse)
+        #cv2.flipで画像を左右反転
+        if src is not None:
+            img = cv2.flip(src, 1)
+            cv2.imwrite(image_dir + image + '_in' + '.jpg', img)
 
-    labelfile = str(label_dir + image + '.txt')
+        #元画像のラベルファイルを取り出し
+        labelfile = str(label_dir + image + '.txt')
+        newlabel = str(label_dir + image + '_in.txt')
+        shutil.copyfile(labelfile, label_dir + image + "_in.txt")
 
-    n, x, y, w, h = [], [], [], [], []
-    with open(labelfile, 'r') as fp:
-        for line in fp: 
-            data = re.split(" ", line)
+        num, x_point, y_point, width, height = [], [], [], [], []
+        with open(newlabel, 'r+') as fp:
+            for line in fp: 
+                data = re.split(" ", line)
 
-            n.append(str(data[0])) 
-            x.append(float(data[1]))
-            y.append(str(data[2]))
-            w.append(str(data[3]))
-            h.append(str(data[4]))
+                num.append(str(data[0])) 
+                x_point.append(float(data[1]))
+                y_point.append(str(data[2]))
+                width.append(str(data[3]))
+                height.append(str(data[4]))
 
-    print(x)
-    x_beyond = []
+            x_beyond = []
 
-    for xl in x:
-        xl = abs(xl*640-640)/640
-        x_beyond.append(str(round(xl, 7)))
+            for x in x_point:
+                x = abs(x*640-640)/640
+                x_beyond.append(str(round(x, 7)))
 
-    newlabel = str(label_dir + image + '_in.txt')
-
-    with open(newlabel, 'w') as fp:
-        for i, nu in enumerate(n):
-            fp.write(nu + ' ' + x_beyond[i] + ' ' + y[i] + ' ' + w[i] + ' ' + h[i])
+            for (n, x, y, w, h) in zip(num, x_beyond, y_point, width, height):
+                fp.write(n + ' ' + x + ' ' + y + ' ' + w + ' ' + h)
     
-                    
+if __name__ == "__main__":
+
+    reverse()
