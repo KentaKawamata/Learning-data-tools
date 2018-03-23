db541ca419dd3cdf921a69ea557c7ef03d90b307
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-23 21:09:56 +0900

    Renameとreformatを統合

--- src/reformat.py
+++ src/reformat.py
@@ -4,13 +4,19 @@ import path as PATH
 
 def reformat(image_path):
 
-    for PF in glob.iglob(os.path.join(image_path, "*.png")):  
+    PathAndFile = glob.iglob(os.path.join(image_path, "*.png"))
+
+    for i, PF in enumerate(PathAndFile, 1):  
+
+        today = datetime.date.today()
+        #today = today - datetime.timedelta(days=1)
 
         #画像データの名前のみ取り込み
         title, ext = os.path.splitext(os.path.basename(PF))
 
         IMG = cv2.imread(title + '.png')
-        cv2.imwrite(os.path.join(image_path, title + '.jpg'), IMG)
+        newname = str(today) + '_' + str("{0:04d}".format(i))
+        cv2.imwrite(image_path + newname + '.jpg', IMG)
 
 if __name__ == '__main__':
 
