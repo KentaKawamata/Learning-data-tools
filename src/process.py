26427f734bd75c859ed7d7bbe6fdcae1ec84cb6a
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-23 22:59:24 +0900

    ファイルパスxmlファイルに統合(途中)

--- src/process.py
+++ src/process.py
@@ -1,31 +1,38 @@
 import glob, os
 import cv2
+import path as PATH
 
-# Current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+def process(image_path):
 
-# Directory where the data will reside, relative to 'darknet.exe'
-path_data = '/mnt/dataset/Boll_YOLO/convert/images/'
+    # Current directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Percentage of images to be used for the test set
-percentage_test = 10;
+    # Percentage of images to be used for the test set
+    percentage_test = 10;
 
-# Create and/or truncate train.txt and test.txt
-file_train = open('train.txt', 'w')  
-file_test = open('test.txt', 'w')
+    # Create and/or truncate train.txt and test.txt
+    file_train = open(os.path.join(image_path, 'train.txt'), 'w')  
+    file_test = open(os.path.join(image_path, 'test.txt'), 'w')
 
-# Populate train.txt and test.txt
-counter = 1  
-index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):  
-    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    # Populate train.txt and test.txt
+    counter = 1  
+    index_test = round(100 / percentage_test)  
+    for PF in glob.iglob(os.path.join(image_path, "*.png")):  
 
-    IMG = cv2.imread(title + '.png')
-    cv2.imwrite(title + '.jpg', IMG)
+        title, ext = os.path.splitext(os.path.basename(PF))
 
-    if counter == index_test:
-        counter = 1
-        file_test.write(path_data + title + '.jpg' + "\n")
-    else:
-        file_train.write(path_data + title + '.jpg' + "\n")
-        counter = counter + 1
+        IMG = cv2.imread(title + '.png')
+        cv2.imwrite(title + '.jpg', IMG)
+
+        if counter == index_test:
+            counter = 1
+            file_test.write(image_path + title + '.jpg' + "\n")
+        else:
+            file_train.write(image_path + title + '.jpg' + "\n")
+            counter = counter + 1
+
+if __name__ == "__main__":
+
+    paths = PATH.file_path()
+
+    process(paths[0])
