f8b374f35de3077b7a7e2166cb8cd1b96a8340ed
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-03-24 12:32:07 +0900

    path.py修正

--- src/path.py
+++ src/path.py
@@ -7,5 +7,7 @@ def file_path():
 
     image_path = root.findtext("image_path")
     label_path = root.findtext("label_path")
+    temp_image = root.findtext("temp_image")
+    temp_label = root.findtext("temp_label")
 
-    return image_path, label_path
+    return image_path, label_path, temp_image, temp_label
