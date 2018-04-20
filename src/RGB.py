852c3f7edcceb1c89564d0631f53825675bda391
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-04-20 04:48:52 -0700

    add RGB

---
+++ src/RGB.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import glob, os, sys, re, shutil
+import cv2
+import path as PATH
+from tqdm import tqdm
+
+class Lumine():
+    def __init__(self, Ipath, Lpath):
+        self.num = 0
+        self.src = None
+        self.image_path = Ipath
+        self.label_path = Lpath
+        self.images = []
+        self.pbar = None
+        self.img = None
+        self.image_name = None
+        self.img = None
+
+    def mashi(self):
+
+        print("------start multiply images------")
+        self.images = glob.iglob(os.path.join(self.image_path, "*"))
+        self.count(self.images)
+        self.images = glob.iglob(os.path.join(self.image_path, "*"))
+
+        for i, PF in enumerate(tqdm(self.images)):
+            self.image_name, ext = os.path.splitext(os.path.basename(PF))
+
+            self.read_image(ext)
+
+            if self.src is not None:
+                #############
+
+            self.pbar.update(1)
+
+        print("\n------end multiply images------")
+
+    def read_image(self, f):
+        if str(f) == ".png" or str(f) == ".jpg":
+            self.src = cv2.imread(os.path.join(self.image_path, self.image_name+f), 1)
+        else:
+            self.src = None
+
+    def RGB(self):
+        b,g,r = cv2.split(self.src)
+       
+        img_blue_c3 = b*0.4
+        img_green_c3 = g
+        img_red_c3 = r*0.8
+
+        img_c3 = cv2.merge((img_blue_c3, img_green_c3, img_red_c3))
+
+        cv2.imwrite(self.image_name + "_RGB.jpg", img_c3)
+        self.add_label(self.image_name + "_RGB.jpg")
+
+
+    def count(self, files):
+        for i, P in enumerate(files):
+            self.num = self.num + 1
+        self.pbar = tqdm(total=int(self.num))
+
+    def write_image(self, signal):
+        new_name = self.image_name + signal
+        cv2.imwrite(os.path.join(self.image_path, new_name + '.jpg'), self.img)
+        self.add_label(new_name)
+
+    def add_label(self, new_filename):
+        shutil.copyfile(self.label_path + self.image_name + '.txt', self.label_path + new_filename + ".txt")
+
+    def normalization(self, hensa, avrage, signal):
+        self.img = (self.src-np.mean(self.src))/np.std(self.src)*hensa+avrage
+        self.write_image(signal)
+
+    def median(self):
+        average_square = (10, 10)
+        self.img = cv2.blur(self.src, average_square)
+        self.write_image('_M')
+
+    def gauss(self):
+        row, col, ch = self.src.shape
+        mean = 0
+        sigma = 15
+        gauss = np.random.normal(mean, sigma, (row, col, ch))
+        gauss = gauss.reshape(row, col, ch)
+        self.img = self.src + gauss
+        self.write_image('_G')
+
+if __name__ == "__main__":
+
+    paths = PATH.file_path()
+
+    mizumashi = Lumine(paths[0], paths[1])
+    mizumashi.mashi()
--- src/image_generator.py
+++ src/image_generator.py
@@ -3,33 +3,42 @@ import glob
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
 from PIL import Image
-import cv2 
+import cv2
+import path as PA
+
 def draw_images(generator, x, dir_name, index):
     # 出力ファイルの設定
     save_name = 'images-' + str(index)
-    g = generator.flow(x, batch_size=1, save_to_dir=output_dir, save_prefix=save_name, save_format='jpg')
+    g = generator.flow(x, batch_size=1, save_to_dir=dir_name, save_prefix=save_name, save_format='jpg')
 
     # 1つの入力画像から何枚拡張するかを指定
     # g.next()の回数分拡張される
-    for i in range(10):
+    for i in range(2):
         bach = g.next()
+
 if __name__ == '__main__':
-    # 出力先ディレクトリの設定
-    output_dir = "extended"
-    if not(os.path.exists(output_dir)):
-         os.mkdir(output_dir)
-    images = glob.glob('*.jpg')
-        
+
+    paths = PA.file_path()
+
+    #if not(os.path.exists(output_dir)):
+    #     os.mkdir(output_dir)
+    #images = glob.glob('*.jpg')
+    images = glob.iglob(os.path.join(paths[0], "*.jpg"))   
+     
+
+
     print(images)
-    generator = ImageDataGenerator(rotation_range=90, # 90°まで回転
-                      width_shift_range=0.1, # 水平方向に
-                      height_shift_range=0.1, # 垂直方向にランダムでシフト
-                      channel_shift_range=50.0, # 色調をランダム変更
-                      shear_range=0.39, # 斜め方向(pi/8まで)に引っ張る
-                      horizontal_flip=True, # 垂直方向にランダムで反転
-                      vertical_flip=True # 水平方向にランダムで反転
+    generator = ImageDataGenerator(rotation_range=0, # 90°まで回転
+                      width_shift_range=0, # 水平方向に
+                      height_shift_range=0, # 垂直方向にランダムでシフト
+                      channel_shift_range=25.0, # 色調をランダム変更
+                      shear_range=0, # 斜め方向(pi/8まで)に引っ張る
+                      horizontal_flip=False, # 垂直方向にランダムで反転
+                      vertical_flip=False # 水平方向にランダムで反転
                       )
+
     for i,image in enumerate(images):
+
         img = Image.open(image)
         print(img,image)
         #img = load_img(img)
@@ -37,4 +46,6 @@ if __name__ == '__main__':
         x = np.asarray(img)
         x = np.expand_dims(x, axis=0)
         print(x.shape)
-        draw_images(generator, x, output_dir, i)
+        draw_images(generator, x, paths[0], i)
+
+
